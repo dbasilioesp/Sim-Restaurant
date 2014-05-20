1985d1336fce521748a2dec1900a8c66a0094979
David Basilio <david.basilio@azion.com>
2014-05-20 08:10:26 -0300

    Create just two thread to gas point

--- gas_point.cpp
+++ gas_point.cpp
@@ -1,83 +1,78 @@
 #include "stdafx.h"
 
-//// testeThreadsC11.cpp : Defines the entry point for the console application.
-//
-//#include <thread>
-//#include <iostream>
-//#include <vector>
-//#include <chrono>
-//#include <mutex>
-//
-//using namespace std;
-//using namespace chrono;
-//
-//
-//
-//mutex mu;
-//time_point<system_clock, system_clock::duration> initialTime;
-//
-//const int MAX_RECEIVERS_GASPOINT = 4;
-//int receivers_gaspoint = MAX_RECEIVERS_GASPOINT;
-//int cntr;
-//
-//void gasPointReceiver(int i){
-//	
-//	cout << "receiving " << i <<  endl;
-//	
-//	this_thread::sleep_for(seconds(4));
-//	
-//	cout << "leaving " << i << endl;
-//}
-//
-//void generator(int id){
-//
-//	vector<thread> threads;
-//
-//	cout << "Initializing generator: " << id << endl;
-//	cout << "Thread Id:" << this_thread::get_id() << endl;
-//
-//	mu.lock();
-//	cntr++;
-//	mu.unlock();
-//
-//	for (int x = 0; x < 3; x++){
-//
-//		if(receivers_gaspoint > 0){
-//
-//			mu.lock();
-//			threads.push_back(thread(gasPointReceiver, receivers_gaspoint));
-//			receivers_gaspoint--;
-//			mu.unlock();
-//
-//			this_thread::sleep_for(seconds(3));
-//		}
-//	}
-//
-//	for (auto& thread : threads){
-//		thread.join();
-//	}
-//}
-//
-//int main(int argc, char* argv[])
-//{
-//	cout << "Gas Point" << endl;
-//	cout << "---------" << endl << endl;
-//
-//	cntr = 0;
-//	initialTime = ::system_clock::now();
-//	
-//	thread t1(generator, 1);
-//	thread t2(generator, 2);
-//	t1.join();
-//	t2.join();
-//
-//	auto end = ::system_clock::now();
-//	auto interval = end - initialTime;
-//
-//	cout << endl << "Time: " << duration_cast<::seconds>(interval).count() << " seconds" << endl;
-//	cout << "Finishing... " << endl;
-//
-//	system("PAUSE");
-//	return 0;
-//}
-//
+// testeThreadsC11.cpp : Defines the entry point for the console application.
+
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <mutex>
+#include <queue>
+
+using namespace std;
+using namespace chrono;
+
+struct Car {
+	int id;
+};
+
+mutex mu;
+time_point<system_clock, system_clock::duration> initialTime;
+queue<Car> cars;
+
+const int MAX_GASPOINT_RECEIVERS = 4;
+int gaspoint_receivers = MAX_GASPOINT_RECEIVERS;
+bool generator_notified = false;
+
+void consumerGasPoint(){
+	
+	cout << "Initializing gas point consumer" <<  endl;
+
+	while(!generator_notified || !cars.empty()){
+
+		this_thread::sleep_for(seconds(3));
+		if(!cars.empty()){
+			cars.pop();
+		}
+	}
+
+}
+
+
+void producerCars(){
+
+	cout << "Initializing generator of cars" <<  endl;
+
+	for (int i = 1; i <= 1; i++){
+		Car car;
+		car.id = i;
+		cars.push(car);
+		this_thread::sleep_for(seconds(2));
+	}
+
+	generator_notified = true;
+}
+
+int main(int argc, char* argv[])
+{
+	cout << "Gas Point" << endl;
+	cout << "---------" << endl << endl;
+
+	initialTime = ::system_clock::now();
+	
+	thread thread_producer_cars(producerCars);
+	thread thread_gaspoint_consumer(consumerGasPoint);
+
+	thread_producer_cars.join();
+	thread_gaspoint_consumer.join();
+	
+	auto end = ::system_clock::now();
+	auto interval = end - initialTime;
+
+	cout << endl << "Time: " << duration_cast<::seconds>(interval).count() << " seconds" << endl;
+	cout << "Finishing... " << endl;
+
+	system("PAUSE");
+	return 0;
+}
+
